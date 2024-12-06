a07ff54f01d8eeeb04c96161ccf0f4caa5602b03
PauloHenrique-UFPI <hunter404paulo@gmail.com>
2024-12-05 23:08:22 -0300

    alterações para reunião na sexta

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-const themes = document.querySelectorAll('.faq-theme');
-const questionsContainers = document.querySelectorAll('.faq-questions');
+const themes = document.querySelectorAll(".faq-theme");
+const questionsContainers = document.querySelectorAll(".faq-questions");
 
-themes.forEach(theme => {
-    theme.addEventListener('click', () => {
-        const selectedTheme = theme.getAttribute('data-theme');
+themes.forEach((theme) => {
+  theme.addEventListener("click", () => {
+    const selectedTheme = theme.getAttribute("data-theme");
 
-        themes.forEach(t => t.classList.remove('active'));
-        questionsContainers.forEach(container => {
-            container.style.display = 'none';
-        });
-
-        theme.classList.add('active');
-        document.getElementById(selectedTheme).style.display = 'block';
+    themes.forEach((t) => t.classList.remove("active"));
+    questionsContainers.forEach((container) => {
+      container.style.display = "none";
     });
+
+    theme.classList.add("active");
+    document.getElementById(selectedTheme).style.display = "block";
+  });
 });
 
-const questions = document.querySelectorAll('.faq-question');
+const questions = document.querySelectorAll(".faq-question");
 
-questions.forEach(question => {
-    question.addEventListener('click', () => {
-        const answer = question.nextElementSibling;
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
 
-        if (answer.style.display === 'block') {
-            answer.style.display = 'none';
-            question.classList.remove('active');
-        } else {
-            answer.style.display = 'block';
-            question.classList.add('active');
-        }
-    });
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+      question.classList.remove("active");
+    } else {
+      answer.style.display = "block";
+      question.classList.add("active");
+    }
+  });
+});
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
 });
